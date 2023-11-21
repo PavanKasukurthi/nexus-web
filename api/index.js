f247f44826d63c65e655b82598f87bd8da196912
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-11-21 22:22:33 +0530

    initial setup

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import Express from 'express'
+const app = Express()
+
+import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
+import likeRoutes from './routes/likes.js'
+import commentRoutes from './routes/comments.js'
+import authRoutes from './routes/auth.js'
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/likes', likeRoutes)
+app.use('/api/comments', commentRoutes)
+
+app.listen(8800, () => {
+  console.log('App is listening!')
+})
