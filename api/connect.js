0528a74b6bcb112d51df7901d66b55002c7703d5
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-11-22 11:23:29 +0530

    added dotenv

---
+++ api/connect.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+import mysql from 'mysql'
+
+export const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+})
--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,3 +1,9 @@
-export const register = (req, res) => {}
+export const register = (req, res) => {
+
+    //CHECK IF USER EXISTS
+
+    //CREATE A NEW USER
+        //HASH THE PASSWORD
+}
 export const login = (req, res) => {}
 export const logout = (req, res) => {}
